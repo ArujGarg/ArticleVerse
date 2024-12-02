4fa160a5146143b1b636cdc2a3eb2b24a1a7fe8d
Aruj Garg <arujgarg@Arujs-MacBook-Air.local>
2024-12-02 21:49:35 +0530

    frontend started

---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,19 @@
+export const Quote = () => {
+    return (
+    <div className="bg-slate-300 h-screen justify-center flex flex-col ">
+        <div className="flex justify-center">
+            <div className="max-w-lg ">
+                <div className=" font-bold text-3xl">
+                    "The customer service i recieved was exceptional. The support team went above and beyond to address my concerns." 
+                </div>
+                <div className="max-w-md text-xl font-semibold mt-3">
+                     Jules WinnField
+                </div>
+                <div className="max-w-md text-sm text-slate-500">
+                    CEO, Acme Inc
+                </div>   
+            </div>
+        </div>
+       
+    </div>
+)}
